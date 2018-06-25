42789f8d78a0c80daa2459e1e72fe5790807003d
bill <bill.schrickel@gmail.com>
2018-06-24 20:14:33 -0500

    Content observer working

--- app/src/main/java/com/bill/android/myapplication/adapters/MovieAdapter.java
+++ app/src/main/java/com/bill/android/myapplication/adapters/MovieAdapter.java
@@ -1,9 +1,13 @@
 package com.bill.android.myapplication.adapters;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +16,7 @@ import android.widget.ImageView;
 
 import com.bill.android.myapplication.R;
 import com.bill.android.myapplication.activities.DetailActivity;
+import com.bill.android.myapplication.database.FavoritesContract;
 import com.bill.android.myapplication.models.Movie;
 import com.squareup.picasso.Picasso;
 
@@ -22,15 +27,23 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
     private Context mContext;
     private ArrayList<Movie> mMovieList;
+    private Observer mObserver;
 
     public MovieAdapter(Context context, ArrayList<Movie> itemList) {
         mContext = context;
         this.mMovieList = itemList;
+        mObserver = new Observer(new Handler());
     }
 
     @Override
     public int getItemCount() {
         return mMovieList == null ? 0 : mMovieList.size();
+    }
+
+    public void addData(ArrayList<Movie> movies) {
+        mMovieList.clear();
+        mMovieList.addAll(movies);
+        notifyDataSetChanged();
     }
 
     @Override
@@ -50,6 +63,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         holder.poster.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                mContext.getContentResolver().registerContentObserver(Uri.parse(FavoritesContract.FavoriteEntry.CONTENT_URI.toString() + "/" + movie.getId()), false, mObserver);
                 Intent intent = new Intent(mContext, DetailActivity.class);
                 intent.putExtra("movie_extra", movie);
                 mContext.startActivity(intent);
@@ -64,6 +78,47 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         ViewHolder(View itemView) {
             super(itemView);
             poster = (ImageView) itemView.findViewById(R.id.one_poster);
+        }
+    }
+
+    class Observer extends ContentObserver {
+
+        /**
+         * Creates a content observer.
+         *
+         * @param handler The handler to run {@link #onChange} on, or null if none.
+         */
+        public Observer(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            Log.v("test", "onchange");
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            Long id = null;
+            Log.v("test", uri.toString());
+
+            try {
+                id = ContentUris.parseId(uri);
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+            }
+
+            if (id != null) {
+                for (int i = 0; i < mMovieList.size(); i++) {
+                    if (mMovieList.get(i).getId().equals(String.valueOf(id))) {
+                        mMovieList.remove(i);
+                        break;
+                    }
+                }
+                notifyDataSetChanged();
+            }
         }
     }
 }
--- app/src/main/java/com/bill/android/myapplication/database/FavoritesContentProvider.java
+++ app/src/main/java/com/bill/android/myapplication/database/FavoritesContentProvider.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 // Credit to https://github.com/udacity/android-content-provider
 public class FavoritesContentProvider extends ContentProvider {
@@ -124,6 +125,11 @@ public class FavoritesContentProvider extends ContentProvider {
         switch(match) {
             case FAVORITE:
                 numDeleted = db.delete(FavoritesContract.FavoriteEntry.TABLE_NAME, selection, selectionArgs);
+
+                if (numDeleted > 0) {
+                    Log.v("test", uri.toString() + "/" + selectionArgs[0]);
+                    getContext().getContentResolver().notifyChange(Uri.parse(uri.toString() + "/" + selectionArgs[0]), null);
+                }
                 break;
             case FAVORITE_WITH_ID:
                 numDeleted = db.delete(FavoritesContract.FavoriteEntry.TABLE_NAME,
